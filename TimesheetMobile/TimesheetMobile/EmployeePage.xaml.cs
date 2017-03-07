2c9cd7c3ca900cf638ba4cf3972af03e72fd498b
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-07 15:23:15 +0200

    Ensimmäinen auttava versio työntekijöiden listauksesta.

---
+++ TimesheetMobile/TimesheetMobile/EmployeePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TimesheetMobile
+{
+    public partial class EmployeePage : ContentPage
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            employeeList.ItemsSource = new string[] { "AAA", "BBB" };
+        }
+    }
+}
