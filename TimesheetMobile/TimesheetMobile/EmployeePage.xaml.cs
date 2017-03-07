b82c9218f5d5c3b9a14454b895d7f609a6c38a0a
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-07 15:54:26 +0200

    Toteutettu yksinkertainen työntekijöiden listauksen lataus web-taustajärjestelmästä.

--- TimesheetMobile/TimesheetMobile/EmployeePage.xaml.cs
+++ TimesheetMobile/TimesheetMobile/EmployeePage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,24 @@ namespace TimesheetMobile
             InitializeComponent();
 
             employeeList.ItemsSource = new string[] { "AAA", "BBB" };
+        }
+
+        public async void LoadEmployees(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://pointcol-timesheetmobile.azurewebsites.net");
+                string json = await client.GetStringAsync("/api/employee");
+                string[] employees = JsonConvert.DeserializeObject<string[]>(json);
+
+                employeeList.ItemsSource = employees;
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.GetType().Name + ": " + ex.Message;
+                employeeList.ItemsSource = new string[] { errorMessage };
+            }
         }
     }
 }
