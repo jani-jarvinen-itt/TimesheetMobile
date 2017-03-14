c48ba8d6389b1253e1a14ce6fe928aaa75cdee01
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-14 14:29:43 +0200

    Lisätty työtehtävien kirjaamiseen tehtävän aloitustoiminto.

--- TimesheetMobile/TimesheetMobile/WorkAssignmentPage.xaml.cs
+++ TimesheetMobile/TimesheetMobile/WorkAssignmentPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using TimesheetMobile.Models;
 using Xamarin.Forms;
 
 namespace TimesheetMobile
@@ -35,6 +35,49 @@ namespace TimesheetMobile
                 string errorMessage = ex.GetType().Name + ": " + ex.Message;
                 assignmentList.ItemsSource = new string[] { errorMessage };
             }
+        }
+
+        public async void StartWork(object sender, EventArgs e)
+        {
+            string assignmentName = assignmentList.SelectedItem.ToString();
+            if (assignmentName != "")
+            {
+                try
+                {
+                    WorkAssignmentOperationModel data = new WorkAssignmentOperationModel()
+                    {
+                        Operation = "Start",
+                        AssignmentTitle = assignmentName
+                    };
+
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri("http://pointcol-timesheetmobile.azurewebsites.net");
+                    string input = JsonConvert.SerializeObject(data);
+                    StringContent content = new StringContent(input,Encoding.UTF8,"application/json");
+
+                    HttpResponseMessage message = await client.PostAsync("/api/workassignment",content);
+                    string reply = await message.Content.ReadAsStringAsync();
+                    bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+                    if (success)
+                    {
+                        await DisplayAlert("Start Work", "Work has been started.", "Close");
+                    }
+                    else
+                    {
+                        await DisplayAlert("Start Work", "Could not start work.", "Close");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string errorMessage = ex.GetType().Name + ": " + ex.Message;
+                    assignmentList.ItemsSource = new string[] { errorMessage };
+                }
+            }
+        }
+
+        public async void StopWork(object sender, EventArgs e)
+        {
         }
     }
 }
