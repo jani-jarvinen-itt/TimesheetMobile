8417a6b8524921ed8bac1a27865ea1be4c8e8e8f
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-03-21 14:52:34 +0200

    Lisätty toiminto, jolla voidaan näyttää tietokantaan tallennettu työntekijän kuva.

--- TimesheetMobile/TimesheetMobile/EmployeePage.xaml.cs
+++ TimesheetMobile/TimesheetMobile/EmployeePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -17,6 +18,31 @@ namespace TimesheetMobile
             InitializeComponent();
 
             employeeList.ItemsSource = new string[] { "" };
+            employeeList.ItemSelected += EmployeeList_ItemSelected;
+        }
+
+        private async void EmployeeList_ItemSelected(object sender,
+            SelectedItemChangedEventArgs e)
+        {
+            string employee = employeeList.SelectedItem?.ToString();
+            if (!string.IsNullOrEmpty(employee))
+            {
+                try
+                {
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri("http://pointcol-timesheetmobile.azurewebsites.net");
+                    string json = await client.GetStringAsync("/api/employee?employeeName="+ employee);
+                    byte[] imageBytes = JsonConvert.DeserializeObject<byte[]>(json);
+
+                    employeeImage.Source = ImageSource.FromStream(
+                        () => new MemoryStream(imageBytes));
+                }
+                catch (Exception ex)
+                {
+                    string errorMessage = ex.GetType().Name + ": " + ex.Message;
+                    employeeList.ItemsSource = new string[] { errorMessage };
+                }
+            }
         }
 
         public async void LoadEmployees(object sender, EventArgs e)
