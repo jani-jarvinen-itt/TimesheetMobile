4bcf76738ca85fc9ebd8b1b8cd7d0b3624018c6a
Jani Järvinen <jani.jarvinen@intertechno.org>
2017-06-07 13:33:32 +0300

    Lisätty kameran kuvanotto toteutus Android-projektiin sekä tehty Portable-projektiin yksinkertainen käyttöliittymä kuvan ottamiselle. Portable-projekti osaa kutsua Android-toteutusta Xamarinin DependencyServicen kautta.

---
+++ TimesheetMobile/TimesheetMobile.Droid/Camera.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TimesheetMobile.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Camera))]
+
+namespace TimesheetMobile.Droid
+{
+    public class Camera : TimesheetMobile.ICamera
+    {
+        public void TakePicture(string employeeName)
+        {
+            MainActivity.AndroidMainActivity.TakeAPicture(null, null);
+        }
+    }
+}
